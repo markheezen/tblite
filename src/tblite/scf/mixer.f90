d0e66e56e9564fab9ea14f62bba81c9050c8f00c
Mark Heezen <heezen@pc.rwth-aachen.de>
2024-08-15 14:23:55 +0200

    Broyden in correct class structure

--- src/tblite/scf/mixer.f90
+++ src/tblite/scf/mixer.f90
@@ -32,119 +32,72 @@ module tblite_scf_mixer
    implicit none
 
    interface
-   type(c_ptr) function new_broyden(ndim, memory, alpha) bind(C,name="SetupBroyden")
-       use iso_c_binding
-       integer(c_int), value :: ndim
-       integer(c_int), value :: memory
-       real(c_double), value :: alpha
-   end function new_broyden
-
-   integer(c_int) function get_broyden_dimension(charge, dipole, quadrupole, nat, nsh) bind(C,name="GetBroydenDimension")
-      use iso_c_binding
-      integer(c_int), value :: charge
-      integer(c_int), value :: dipole
-      integer(c_int), value :: quadrupole
-      integer(c_int), value :: nat
-      integer(c_int), value :: nsh
-   end function get_broyden_dimension
-
-   subroutine next_broyden(mixer,iter) bind(C,name="NextBroyden")
-      use iso_c_binding
-      type(c_ptr), value, intent(in) :: mixer
-      integer(c_int), value, intent(in) :: iter
-  end subroutine next_broyden
-
-   real(c_double) function get_broyden_error(mixer) bind(C,name="GetBroydenError")
-      use iso_c_binding
-      type(c_ptr), value :: mixer
-   end function get_broyden_error
-
-   subroutine destroy_broyden(mixer) bind(C,name="DestroyBroyden")
-      use iso_c_binding
-      type(c_ptr), value :: mixer
-   end subroutine destroy_broyden
+      type(c_ptr) function new_broyden(ndim, memory, alpha) bind(C,name="SetupBroyden")
+         use iso_c_binding
+         integer(c_int), value :: ndim
+         integer(c_int), value :: memory
+         real(c_double), value :: alpha
+      end function new_broyden
    end interface
 
 contains
 
 !> Create a new instance of the mixer
-subroutine new_mixer(mixer, type, mol, calc, wfn, info)
-   !> Pointer to the mixer on exit
-   type(c_ptr), intent(out) :: mixer
-   !> Type of mixer to use (Broyden=0, DIIS=1)
-   integer, intent(in) :: type
-   !> Molecular structure data
-   type(structure_type), intent(in) :: mol
-   !> Single-point calculator
-   type(xtb_calculator), intent(in) :: calc
-   !> Wavefunction data
-   type(wavefunction_type), intent(inout) :: wfn
-   !> Info data
-   type(scf_info) :: info
-   
-   integer :: ndim
+   subroutine new_mixer(mixer, type, mol, calc, wfn, info)
+      !> Pointer to the mixer on exit
+      type(c_ptr), intent(out) :: mixer
+      !> Type of mixer to use (Broyden=0, DIIS=1)
+      integer, intent(in) :: type
+      !> Molecular structure data
+      type(structure_type), intent(in) :: mol
+      !> Single-point calculator
+      type(xtb_calculator), intent(in) :: calc
+      !> Wavefunction data
+      type(wavefunction_type), intent(inout) :: wfn
+      !> Info data
+      type(scf_info) :: info
 
-   select case(type)
-      case(0)
-         ndim = wfn%nspin*get_broyden_dimension(info%charge, info%dipole, info%quadrupole, mol%nat, calc%bas%nsh)
-         mixer = new_broyden(ndim, calc%max_iter, calc%mixer_damping)
-      case(1)
+      integer :: ndim
+
+      select case(type)
+       case(0)
+         mixer = new_broyden(get_mixer_dimension(mol,calc%bas,info), calc%max_iter, calc%mixer_damping)
+       case(1)
          write(*,*) "DIIS mixer"
+         ! mixer = new_diis(10, calc%max_iter, calc%mixer_damping)
          stop
-   end select
-end subroutine new_mixer
+      end select
+   end subroutine new_mixer
 
-!> Destruct the mixer
-subroutine next_mixer(mixer, type, iter)
-   !> Pointer to the mixer
-   type(c_ptr), intent(in) :: mixer
-   !> Type of mixer to use (Broyden=0, DIIS=1)
-   integer, intent(in) :: type
-   !> Iteration in the SCF process
-   integer, intent(in) :: iter
+!> Get the dimensionality of the mixer
+   function get_mixer_dimension(mol, bas, info) result(ndim)
+      use tblite_scf_info, only : atom_resolved, shell_resolved
+      !> Molecular structure data
+      type(structure_type), intent(in) :: mol
+      !> Basis functions
+      type(basis_type), intent(in) :: bas
+      !> Info data
+      type(scf_info), intent(in) :: info
+      integer :: ndim
 
-   select case(type)
-      case(0)
-         call next_broyden(mixer,iter)
-      case(1)
-         write(*,*) "DIIS mixer"
-         stop
-   end select
-end subroutine next_mixer
+      ndim = 0
 
-!> Get the density error
-function get_error(mixer, type) result(error)
-   !> Pointer to the mixer
-   type(c_ptr), intent(out) :: mixer
-   !> Type of mixer to use (Broyden=0, DIIS=1)
-   integer, intent(in) :: type
-   !> Density error
-   double precision :: error
+      select case(info%charge)
+       case(atom_resolved)
+         ndim = ndim + mol%nat
+       case(shell_resolved)
+         ndim = ndim + bas%nsh
+      end select
 
-   select case(type)
-      case(0)
-         error=get_broyden_error(mixer)
-      case(1)
-         write(*,*) "DIIS mixer"
-         stop
-   end select
-end function get_error
+      select case(info%dipole)
+       case(atom_resolved)
+         ndim = ndim + 3*mol%nat
+      end select
 
-!> Destruct the mixer
-subroutine destroy_mixer(mixer, type)
-   !> Pointer to the mixer
-   type(c_ptr), intent(out) :: mixer
-   !> Type of mixer to use (Broyden=0, DIIS=1)
-   integer, intent(in) :: type
-
-   select case(type)
-      case(0)
-         call destroy_broyden(mixer)
-      case(1)
-         write(*,*) "DIIS mixer"
-         stop
-   end select
-end subroutine destroy_mixer
-
+      select case(info%quadrupole)
+       case(atom_resolved)
+         ndim = ndim + 6*mol%nat
+      end select
+   end function get_mixer_dimension
 
 end module tblite_scf_mixer
